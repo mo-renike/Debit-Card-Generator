54ca4d7b51ee567653445bb2582d63e30819bccd
Morenike Oyewole <herroyalpianist@gmail.com>
2021-10-19 08:56:43 +0100

    wrote function to display exp date, name and card number to DOM

--- script.js
+++ script.js
@@ -3,51 +3,54 @@ const visaCard = document.querySelector("#visaCard");
 const select = document.querySelector("#select");
 const userName = document.querySelector("#name");
 const form = document.querySelector("#form");
-const date = document.querySelector("#date");
-const number = document.querySelector("#card-number");
-const cardName = document.querySelector(".cardName");
+const dates = document.querySelectorAll("#date");
+const numberMaster = document.querySelector("#master-number");
+const numberVisa = document.querySelector("#visa-number");
+const cardName = document.querySelectorAll(".cardName");
+const container = document.querySelector(".card-container");
 
 let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let imgs = ["img/visa.png", "img/master.png"];
+const date = new Date();
+const month = date.getMonth();
+const newMonth = ` ${month < 10 ? `0${month}` : month}`;
+const year = date.getFullYear() + 3;
+const newYear = year.toString().substr(2);
+
+let mo = "1234567890987655";
+let joy = mo.match(/.{1,4}/g);
+console.log(joy.join(" "));
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let name = userName.value;
+
   if (name && name !== " ") {
-    cardName.innerHTML = name;
-    number.innerHTML = `5399 `;
-    for (let i = 0; i < 12; i++) {
-      number.innerHTML += num[getRandomNum()];
+    for (const names of cardName) {
+      names.innerHTML = name;
+    }
+    for (const exp of dates) {
+      exp.innerHTML = `${newMonth}/${newYear}`;
+    }
+    if (select.value == "master") {
+      masterCard.style.display = "flex";
+      visaCard.style.display = "none";
+      numberMaster.innerHTML = `5399 `;
+      for (let i = 0; i < 12; i++) {
+        numberMaster.innerHTML += num[getRandomNum()];
+      }
+    } else {
+      masterCard.style.display = "none";
+      visaCard.style.display = "flex";
+      numberVisa.innerHTML = `4578 `;
+      for (let i = 0; i < 12; i++) {
+        numberVisa.innerHTML += num[getRandomNum()];
+      }
     }
   } else {
     window.location.reload();
   }
-  function getRandomNum() {
-    return Math.floor(Math.random() * num.length);
-  }
-
-  if (select.value == "master") {
-      
-  } else {
-  }
 });
 
-const cardContainer = document.createElement("div");
-cardContainer.classList.add("card-container");
-
-cardContainer.innerHTML = `
-<div class="card" id="visaCard">
-          <div class="logos">
-            <img src="img/visa.png" />
-            <img src="img/chip.png" class="chip" />
-          </div>
-
-            <div class="details">
-            <div id="cardNumber">
-              <p id="card-number"></p>
-              <i class="fa fa-caret-right"></i>
-            </div>
-            <p id="date">10/24</p>
-            <p class="cardName"></p>
-          </div>
-        </div>`;
+function getRandomNum() {
+  return Math.floor(Math.random() * num.length);
+}
